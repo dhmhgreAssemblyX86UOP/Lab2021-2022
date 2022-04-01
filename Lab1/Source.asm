9e40fb964a047e5dbb1b6b583458f64e6f6522bf
dhmhgre <dhmhgre@gmail.com>
2022-04-01 10:26:55 +0300

    Mean Value 4 bytes array

--- Lab1/Source.asm
+++ Lab1/Source.asm
@@ -2,9 +2,33 @@ TITLE Mean Value
 INCLUDE Irvine32.inc
 
 .data
-
+array DWORD 10,20,30,40,50,60,70,80
 .code
 main PROC
+
+; sum =0;
+; for ( int i =0; i<8; i++ ){
+;	sum += array[i];
+; }
+; average = sum /8;
+; EAX : sum
+; ESI : i
+
+mov EAX,0
+mov ECX,8
+mov ESI,0
+L1:
+ADD EAX,[array+ESI*4]   ; destination = destination + source
+INC ESI
+LOOP L1 
+shr EAX,3
+
+
+
+
+
+
+
 exit
 main ENDP
 END main
