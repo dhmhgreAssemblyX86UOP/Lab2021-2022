4ae8b5312b0fd75b92b6326236c4854fb26755b8
dhmhgre <dhmhgre@gmail.com>
2022-05-30 09:38:00 +0300

    Variance ( Step 2 Function Prologue Epilogue )

--- Variance/Source.asm
+++ Variance/Source.asm
@@ -7,16 +7,43 @@ INCLUDE Irvine32.inc
 
 .code
 Mean PROC
+; prologue
+push  ebp
+mov ebp, esp
+; sub esp, x ( optional )
+pushad ; (requires inspections at the end )
 
-ret
+
+; epilogue
+popad
+mov esp,ebp ; ( cleans local variables )
+pop ebp
+ret ; x (STDCALL)  (valid only for CDECL calling convention )
 Mean ENDP
 
 Variance PROC
+; prologue
+push  ebp
+mov ebp, esp
+; sub esp, x ( optional )
+pushad ; (requires inspections at the end )
 
-ret
+
+; epilogue
+popad
+mov esp,ebp ; ( cleans local variables )
+pop ebp
+ret ; x (STDCALL)  (valid only for CDECL calling convention )
 Variance ENDP
 
 main PROC
+; prologue
+push  ebp
+mov ebp, esp
+; sub esp, x ( optional )
+pushad ; (requires inspections at the end )
+
+
 exit
 main ENDP
 END main
